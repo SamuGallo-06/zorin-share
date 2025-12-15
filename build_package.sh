23b4321ef9577c122e38f88c968fa64987ba6453
Samuele Gallicani <samu.gallicani@gmail.com>
2025-12-15 23:11:23 +0100

    Enhance build_package.sh with additional logging for build process steps

--- build_package.sh
+++ build_package.sh
@@ -4,6 +4,12 @@
 
 VERSION="1.0.6"
 
+echo "Starting build process for Samba Configurator version ${VERSION}..."
 debbuild -S -sa
+echo "Signing the package..."
 debsign ../samba-configurator_${VERSION}_source.changes
+echo "Uploading the package to PPA..."
 dput ppa:samugallo06/software ../samba-configurator_${VERSION}_source.changes
+echo "Building package version ${VERSION}..."
+dpkg-buildpackage -b -us -uc
+echo "Package build and upload complete."
